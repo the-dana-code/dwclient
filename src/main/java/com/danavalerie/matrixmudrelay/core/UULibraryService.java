a2095c99b2cd15055317511502d31e0e178cd446
Dana <dana.coder@icloud.com>
2026-01-20 15:32:18 +0700

    Switch `uu_library.json` loading to use `Files.newInputStream` and adjust file copy in build script accordingly.

--- src/main/java/com/danavalerie/matrixmudrelay/core/UULibraryService.java
+++ src/main/java/com/danavalerie/matrixmudrelay/core/UULibraryService.java
@@ -6,6 +6,8 @@ import com.google.gson.reflect.TypeToken;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class UULibraryService {
@@ -60,11 +62,7 @@ public class UULibraryService {
     }
 
     private void loadMap() {
-        try (InputStream is = getClass().getResourceAsStream("/uu_library.json")) {
-            if (is == null) {
-                System.err.println("UULibrary map JSON not found!");
-                return;
-            }
+        try (InputStream is = Files.newInputStream(Paths.get("uu_library.json"))) {
             Gson gson = new Gson();
             Type listType = new TypeToken<List<Room>>() {}.getType();
             List<Room> rooms = gson.fromJson(new InputStreamReader(is), listType);
