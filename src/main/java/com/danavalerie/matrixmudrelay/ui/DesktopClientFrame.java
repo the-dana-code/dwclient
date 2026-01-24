cce69d3463e4be90477468167dfa3aad21b4a565
Dana <dana.coder@icloud.com>
2026-01-25 02:10:18 +0700

    Remove `Help` menu from `DesktopClientFrame`.

--- src/main/java/com/danavalerie/matrixmudrelay/ui/DesktopClientFrame.java
+++ src/main/java/com/danavalerie/matrixmudrelay/ui/DesktopClientFrame.java
@@ -454,8 +454,6 @@ public final class DesktopClientFrame extends JFrame implements MudCommandProces
         viewMenu.add(zoomSubMenu);
         menuBar.add(viewMenu);
 
-        menuBar.add(buildHelpMenu());
-
         return menuBar;
     }
 
@@ -568,35 +566,6 @@ public final class DesktopClientFrame extends JFrame implements MudCommandProces
         }
     }
 
-    private JMenu buildHelpMenu() {
-        JMenu helpMenu = new JMenu("Help");
-        if (currentBg != null && currentFg != null) {
-            updateMenuTheme(helpMenu, currentBg, currentFg);
-        }
-        JMenuItem aboutItem = new JMenuItem("About");
-        if (currentBg != null && currentFg != null) {
-            updateMenuTheme(aboutItem, currentBg, currentFg);
-        }
-        aboutItem.addActionListener(event -> {
-            javax.swing.JOptionPane.showMessageDialog(this,
-                "Lesa's Discworld MUD client\n" +
-                "Copyright (C) 2026 Dana Reese\n\n" +
-                "This program is free software: you can redistribute it and/or modify\n" +
-                "it under the terms of the GNU General Public License as published by\n" +
-                "the Free Software Foundation, either version 3 of the License, or\n" +
-                "(at your option) any later version.\n\n" +
-                "This program is distributed in the hope that it will be useful,\n" +
-                "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
-                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
-                "GNU General Public License for more details.\n\n" +
-                "You should have received a copy of the GNU General Public License\n" +
-                "along with this program.  If not, see <https://www.gnu.org/licenses/>.",
-                "About",
-                javax.swing.JOptionPane.INFORMATION_MESSAGE);
-        });
-        helpMenu.add(aboutItem);
-        return helpMenu;
-    }
 
     private void showFontDialog() {
         Font current = outputPane.getFont();
